c3a3585ad4ec158ff4032c184d3fbb550312ae0e
Anwesh43 <anweshthecool0@gmail.com>
2016-12-24 04:37:10 +0530

    Using proper method for port

--- server_api.js
+++ server_api.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var app = express()
 var BillBoard = require('billboard-hot-100')
+var port = process.env.PORT||8080
 BillBoard.init().then(function(billboard){
   var bodyParser = require('body-parser')
   var mainRouter = require('./router/main-router')(billboard)
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended:true}))
   app.use('/api',mainRouter)
-  app.listen(8080,()=>{
+  app.listen(port,()=>{
     console.log("started server")
   })
 }).catch(function(err){
